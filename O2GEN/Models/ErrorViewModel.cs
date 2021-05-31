995a60a6f22029eb216c57f0dcb9e99feda0c22a
Вадим Еремин <vad510@gmail.com>
2021-05-31 09:01:28 +0300

    Добавьте файлы проекта.

---
+++ O2GEN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace O2GEN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
